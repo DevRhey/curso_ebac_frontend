da6288ad538422b7519344e628b486f235d67e66
DevRhey <devrhey@gmail.com>
2024-02-01 18:13:21 -0300

    Final

--- main.ts
+++ main.ts
@@ -1,7 +1,11 @@
-function multiplication(number1: number, number2: number): number {
-    return number1 * number2;
+function multiplication(
+  number1: number,
+  number2: number,
+  number3: number
+): number {
+  return number1 * number2 * number3;
 }
 
 function saudacao(nome: string): string {
-    return `Olá ${nome}`
+  return `Olá ${nome}`;
 }
